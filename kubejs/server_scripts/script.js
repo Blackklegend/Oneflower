790b09d5b1f6944980b251c8ff426b205382b9bf
Batatobola <70458165+batatob0la@users.noreply.github.com>
2022-01-22 23:22:14 -0300

    Added adobe and other things

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -16,23 +16,47 @@ onEvent('item.tags', event => {
 })
 
 onEvent('recipes', event => {
+
 	//Generic recipe remove
-	for (let remove of ['minecraft:blast_furnace']) {
+	for (let remove of ['minecraft:blast_furnace', 'minecraft:shears']) {
 		event.remove({output: remove})
 	}
-	
-	//Ingots
+
+	//Shortcuts
+	let blockCraft4 = (output, input) => {
+		event.shaped(output, [
+			'II',
+			'II'
+		], {
+			I: input
+		})
+	}
+	let unblockCraft4 = (output, input) => {
+		event.shapeless('4x ' + output, input)
+	}
+
+	let pureDaisy = (output, input) => {
+		event.custom({
+			"type": "botania:pure_daisy",
+			"input": {
+				"type": "block",
+				"block": input
+			},
+			"output": {
+				"name": output
+			}
+			})
+	}
 
 	//?Botania petal blocks
 	for (let colors of ['white', 'orange', 'magenta', 'light_blue', 'yellow', 'lime', 'pink', 'gray', 'light_gray', 'cyan', 'red', 'black', 'blue', 'purple', 'brown', 'green' ]) {
 		event.remove({output: 'botania:' + colors + '_petal_block'})
-		event.shaped('botania:' + colors + '_petal_block', [
-			'PP',
-			'PP'
-		], {
-			P: 'botania:' + colors + '_petal'
-		})
+		blockCraft4('botania:' + colors + '_petal_block', 'botania:' + colors + '_petal')
+		event.remove({output: 'botania:' + colors + '_petal', input: /petal_block/})
+		unblockCraft4('botania:' + colors + '_petal', 'botania:' + colors + '_petal_block')
 	}
+
+	//Generic recipe add
 
 	//Copper blast furnace
 	event.shaped('minecraft:blast_furnace', [
@@ -45,4 +69,51 @@ onEvent('recipes', event => {
 		F: 'minecraft:furnace',
 		B: 'minecraft:smooth_stone'
 	})
+
+	//Adobe
+	event.custom({"type": "interactio:item_fluid_transform",
+		"inputs": [
+			{
+				"item":  "minecraft:dirt",
+				"count": 1
+			},
+			{
+				"item": "minecraft:grass",
+				"count": 1
+			}
+		],
+		"fluid": {"fluid": "water"},
+		"output": {
+			"item": "oneflower:adobe_blend",
+			"count": 4
+		},
+		"consume_fluid": 0.3333
+	})
+
+	event.campfireCooking('minecraft:brick', 'minecraft:clay_ball').cookingTime(400)
+	event.campfireCooking('oneflower:adobe_brick', 'oneflower:adobe_blend').cookingTime(400)
+	blockCraft4('oneflower:adobe_bricks', 'oneflower:adobe_brick')
+	unblockCraft4('oneflower:adobe_brick', 'oneflower:adobe_bricks')
+
+	//Shears
+	event.shaped('minecraft:shears', [
+		'SI',
+		'NS'
+	], {
+		S: 'minecraft:stick',
+		I: 'minecraft:iron_ingot',
+		N: 'minecraft:iron_nugget'
+	})
+	event.shaped('oneflower:wooden_shears', [
+		'SP',
+		'BS'
+	], {
+		S: '#forge:rods/wooden',
+		P: '#minecraft:planks',
+		B: '#minecraft:wooden_buttons'
+	})
+
+	//Pure daisy lava-ice
+	pureDaisy('minecraft:lava', 'botania:red_petal_block')
+	pureDaisy('minecraft:ice', 'botania:light_blue_petal_block')
 })
--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -1,11 +1,25 @@
 // priority: 0
 
 onEvent('item.registry', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
+	//Generic item add
+	let itemI = ['oneflower:adobe_blend', 'oneflower:adobe_brick']
+	let itemN = ['Adobe Blend', 'Adobe Brick']
+	for (let i = 0; i < itemI.length; i++) {
+		event.create(itemI[i]).displayName(itemN[i])
+	}
+
+	//Wooden shears
+	event
+		.create('oneflower:wooden_shears')
+		.displayName('Wooden Shears')
+		.unstackable()
+		.burnTime(500)
+		.tool('shears', 1)
+		.maxDamage(188);
 })
 
 onEvent('block.registry', event => {
+	//Mystical bush
 	event
 		.create('fabio:mystical_bush')
 		.type('basic')
@@ -14,5 +28,15 @@ onEvent('block.registry', event => {
 		.displayName('Mystical Bush')
 		.notSolid()
 		.renderType('cutout')
+		.defaultCutout()
 		.noItem();
+
+	//Adobe bricks
+	event
+		.create('oneflower:adobe_bricks')
+		.displayName('Adobe Bricks')
+		.hardness(1)
+		.harvestTool('pickaxe', 0)
+		.requiresTool(true)
+		.resistance(6);
 })
